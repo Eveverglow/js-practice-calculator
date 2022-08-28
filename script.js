7a63195aca25318d66f7a9be24bfd7242b13e6db
Viktoryia Danko <viktoryia.danko@LeverX.local>
2022-08-28 12:52:33 +0300

    change html

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+
+const operationButtons = document.querySelectorAll("[data-operation]");
+
+const equalsButton = document.querySelector("[data-equals]");
+
+console.log(numberButtons, operationButtons, equalsButton);
